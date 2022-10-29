5bce6be4c48bc99f51044f871bcbb5afd3805b62
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-29 17:12:59 +0100

    initialise translate=page branch

--- src/components/translation/translate.jsx
+++ src/components/translation/translate.jsx
@@ -1,4 +1,8 @@
 import "./translate.scss";
 
-const Translate = () => <section className="translate"></section>;
+const Translate = () => (
+  <section className="translate">
+    <h1>Translate</h1>
+  </section>
+);
 export default Translate;
