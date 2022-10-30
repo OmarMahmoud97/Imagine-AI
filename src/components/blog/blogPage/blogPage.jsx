3244b96d89fbfad0dafee0534ea0a5f44a827374
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-30 20:55:20 +0000

    add blog functionality

--- src/components/blog/blogPage/blogPage.jsx
+++ src/components/blog/blogPage/blogPage.jsx
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { auth, db } from "../../../firebase";
 
-function Blog({ isAuth }) {
+function Blog(user) {
   const [postLists, setPostList] = useState();
   const postsCollectionRef = collection(db, "blogPosts");
+
+  const getPosts = async () => {
+    const data = await getDocs(postsCollectionRef);
+    setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
 
   const deletePost = async (id) => {
     const postDoc = doc(db, "blogPosts", id);
     await deleteDoc(postDoc);
+    getPosts();
   };
-  useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
-      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
 
+  useEffect(() => {
     getPosts();
   }, []);
 
@@ -35,7 +37,10 @@ function Blog({ isAuth }) {
                 <h1> {post.title}</h1>
               </div>
               <div className="deletePost">
-                {isAuth && post.author.id === auth.currentUser.uid && (
+                {console.log(post.author.id)}
+                {console.log(auth.currentUser.uid)}
+                {console.log(user)}
+                {user && post.author.id === auth.currentUser.uid && (
                   <button
                     onClick={() => {
                       deletePost(post.id);
