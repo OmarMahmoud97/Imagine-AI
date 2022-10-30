20fc56f61124c8d34aa9afa5af831961cf3a4bfb
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-30 02:45:09 +0000

    add google o auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from "./components/landing/landing";
 import SignInPage from "./components/login/signInPage/signInPage";
 import NavPage from "./components/navbar/navbar";
 import Translate from "./components/translation/translate";
+import User from "./components/user/user";
 import { AuthContextProvider } from "./context/AuthContext";
 import "./styles/partials/_resets.scss";
 function App() {
@@ -15,6 +16,8 @@ function App() {
             <Route path="/" element={<Landing />} />
             <Route path="/translate" element={<Translate />} />
             <Route path="/SignIn" element={<SignInPage />} />
+            <Route path="/user" element={<User />} />
+
             {/* <Route path="/flight" element={<Flight />} />
           <Route path="/hotel" element={<Hotel />} />
           <Route path="/restaurant" element={<Restaurant />} /> */}
