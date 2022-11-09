876751a4338c90dae682db4f936d3c20bfcccfa5
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-11-09 21:26:49 +0000

    final styling changes

--- src/components/VoiceToText/VoiceToText.jsx
+++ src/components/VoiceToText/VoiceToText.jsx
@@ -1,4 +1,5 @@
 import "./VoiceToText.scss";
+import mic from "../../assets/icons/microphone-thin-240.png";
 
 export default function VoiceTotext(props) {
   return (
@@ -17,12 +18,12 @@ export default function VoiceTotext(props) {
             </div>
           </div>
           <div className="voiceToText__button">
-            <button
+            <div
               onClick={props.onClick}
               className="voiceToText__speak nav__login"
             >
-              Click & Speak!
-            </button>
+              <img className="voiceToText__mic" src={mic} alt="" />
+            </div>
           </div>
         </div>
       </section>
